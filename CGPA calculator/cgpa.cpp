579e46c1ea09a07c8070789f67b97ad7d7e1a7ec
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-08-16 22:47:50 +0530

    CGPA calculator completed

--- CGPA calculator/cgpa.cpp
+++ CGPA calculator/cgpa.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-int main(){
-    cout << "Cgpa Calculator";
+int main()
+{
+    cout << "Cgpa Calculator" << endl;
+    int numSubjects;
+
+    string name;
+    cout << "Enter the name :" << endl;
+    getline(cin, name);
+    // cin>>name;
+
+    cout << "Enter the number of subjects : " << endl;
+    cin >> numSubjects;
+    vector<int> credits(numSubjects);
+    vector<float> grades(numSubjects);
+    for (int i = 0; i < numSubjects; i++)
+    {
+        cout << "Subject " << i + 1 << ":\n";
+        cout << "Credits: ";
+        cin >> credits[i];
+        cout << "Grade (out of 10): ";
+        cin >> grades[i];
+    }
+    float totalGrades = 0;
+    int totalCredits = 0;
+    for (int i = 0; i < numSubjects; ++i)
+    {
+        totalGrades += grades[i] * credits[i];
+        totalCredits += credits[i];
+    }
+    float cgpa = totalGrades / totalCredits;
+    cout << "Your CGPA is: " << cgpa << endl;
+    return 0;
 }
